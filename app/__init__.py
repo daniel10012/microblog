c5c63cc14da02dc4f2a08a6ca875de619d8b1214
Daniel Wegmann <daniel.wegmann@gmail.com>
2019-12-14 21:31:26 +0100

    added email password recovery

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login = LoginManager(app)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
